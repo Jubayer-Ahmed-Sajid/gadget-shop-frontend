68d66d46a10f031a319dd09ebaeb954a526b2b0d
jubayer-ahmed-sajid <jubayerahmed661ac@gmail.com>
2024-12-14 14:18:56 +0600

    first commit

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 className='text-center text-red-600 text-2xl font-bold'>This is home page</h2>
+        </div>
+    );
+};
+
+export default Home;
